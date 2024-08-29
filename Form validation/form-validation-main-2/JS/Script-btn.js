15431a41f6d7791ec55d1fa98c3ce73810bf2d4f
fathima Wafa <162957845+iswza123@users.noreply.github.com>
2024-08-29 13:59:58 -0700

    Add files via upload

---
+++ Form validation/form-validation-main-2/JS/Script-btn.js
@@ -0,0 +1,63 @@
+function validation(){
+    let isValid=true;
+    if(document.getElementById('name').value==""){
+        document.getElementById('er1').style.display="block";
+        isValid=false;
+    }else{
+        document.getElementById('er1').style.display="none";
+    }
+    if(document.getElementById('email').value==""){
+        document.getElementById('er2').style.display="block";
+        isValid=false;
+    }else{
+        document.getElementById('er2').style.display="none";
+    }
+    if(document.getElementById('password').value==""){
+        document.getElementById('er3').style.display="block";
+        isValid=false;
+    }else{
+        document.getElementById('er3').style.display="none";
+    }
+    if(document.getElementById('address').value==""){
+        document.getElementById('er4').style.display="block";
+        isValid=false;
+    }else{
+        document.getElementById('er4').style.display="none";
+    }
+    const radiobtns=document.getElementsByName('gender');
+    let isGenderSelected=false;
+    for(let i=0;i<radiobtns.length;i++){
+        if(radiobtns[i].checked){
+            isGenderSelected=true;
+            break;
+        }
+    }
+    if(!isGenderSelected){
+        document.getElementById('er5').style.display="block";
+        isValid=false;
+
+    }else{
+        document.getElementById('er5').style.display="none";
+    }
+
+    const checkboxs=document.getElementsByName('check');
+    let isHobbyChecked=false;
+    for(let i=0;i<checkboxs.length;i++){
+        if(checkboxs[i].checked){
+            isHobbyChecked=true;
+            break;
+        }
+    }
+    if(!isHobbyChecked){
+        document.getElementById('er6').style.display="block";
+        isValid=false;
+    }
+    else{
+        document.getElementById('er6').style.display="none";
+    }
+    if(isValid){
+        document.getElementById('myForm').submit();
+        alert("Form Submitted SuccessFully");
+    }
+
+}
